a835e7d8c61ed9754adbf1f918dc856dd1a408eb
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2020-01-15 12:31:57 -0300

    Projeto criado

---
+++ src/entities/Product.java
@@ -0,0 +1,34 @@
+/*Fazer um programa para ler os dados de um produto em estoque (nome, preço e quantidade no estoque).Em seguida: •
+ *  Mostrar os dados do produto (nome, preço, quantidade no estoque, valor total no estoque) • 
+ *  Realizar uma entrada no estoque e mostrar novamente os dados do produto • Realizar uma saída no estoque e mostrar
+ *  novamente os dados do produto
+*/ 
+
+package entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+
+	public double totalValueStock() {
+		return price * quantity;
+	}
+
+	public void addProducts(int quantity) { /*incrementar o quantity*/
+		this.quantity += quantity;
+	}
+
+	public void removeProducts(int quantity) { /*decrementar o quantity*/
+		this.quantity -= quantity;
+	}
+
+	@Override
+	public String toString() {
+		return name + ", $ " + String.format("%.2f", price) + ", " + quantity + " units, Total: $"
+				+ String.format("%.2f", totalValueStock());
+
+	}
+
+}
